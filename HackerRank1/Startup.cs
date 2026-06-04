64374182475c6459b37dc06b66d1e60a5c54c074
Yeisson <yeissonalberto14@gmail.com>
2026-06-03 20:28:39 -0600

    Swagger

--- HackerRank1/Startup.cs
+++ HackerRank1/Startup.cs
@@ -81,15 +81,14 @@ namespace LibraryService.WebAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+           
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "LibraryService API v1");
                 });
-            }
+            
 
             app.UseRouting();
             app.UseCors("Frontend");
